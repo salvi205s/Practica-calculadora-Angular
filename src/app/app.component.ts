2736bf0ea1c5addb9ae281f2479491361f6a4714
salvi205 <salvi205s@gmail.com>
2022-12-15 11:13:37 +0100

    Clase 12 y 13 practica calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculadora';
+  title = 'Mini calculadora';
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  /* Para que no concatene, se declara los numeros tipo string y se parsea dentro de suma numero1: String = 0;
+      y los inputs del html tipo text funciona.
+      o se ponen los inputs tipo number*/
+  sumar(): void {
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  resta(): void {
+    this.resultado = this.numero1 - this.numero2;
+  }
 }
